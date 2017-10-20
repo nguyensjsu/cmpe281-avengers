697f98c0902eb84c8bd6ab89b092de9a26914d6f
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-10-20 12:43:12 -0700

    rest endpoint for POST method

--- Product Catalog/src/restful_product.py
+++ Product Catalog/src/restful_product.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+import json
 
 app = Flask(__name__)
 
@@ -10,12 +10,22 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/books'
 
 mongo = PyMongo(app)
 
-@app.route('/books', methods=['GET'])
-def get_all_books():
+@app.route('/books', methods=['GET','POST'])
+def books():
     books = mongo.db.books_collection
-    output = books.find()
-    data = dumps(output)
-    return jsonify({"Status": "OK", "data": data})
+    if request.method == "GET":
+        output = books.find()
+        data = dumps(output)
+        return jsonify({"Status": "OK", "data": data})
+    elif request.method =="POST":
+        book = {}
+        result = json.loads(request.get_data(as_text=True))
+        book['title']=result['title']
+        book['author'] = result['author']
+        book['price']=result['price']
+        output = books.insert(book)
+        data = dumps(output)
+        return jsonify({"Status": "OK", "data": data})
 
 @app.route('/books/title/<title>', methods=['GET'])
 def get_book_by_title(title):
