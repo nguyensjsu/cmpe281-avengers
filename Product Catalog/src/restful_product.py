8732f451d86b779d7b8746c936a178aeefeeb345
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-10-21 22:51:38 -0700

    Updated api endpoint for DELETE request

--- Product Catalog/src/restful_product.py
+++ Product Catalog/src/restful_product.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 import json
+import bson
+from bson.objectid import ObjectId
+from bson import json_util
 
 app = Flask(__name__)
 
@@ -10,22 +13,24 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/books'
 
 mongo = PyMongo(app)
 
-@app.route('/books', methods=['GET','POST'])
+@app.route('/books', methods=['GET',])
 def books():
     books = mongo.db.books_collection
-    if request.method == "GET":
-        output = books.find()
-        data = dumps(output)
-        return jsonify({"Status": "OK", "data": data})
-    elif request.method =="POST":
-        book = {}
-        result = json.loads(request.get_data(as_text=True))
-        book['title']=result['title']
-        book['author'] = result['author']
-        book['price']=result['price']
-        output = books.insert(book)
-        data = dumps(output)
-        return jsonify({"Status": "OK", "data": data})
+    output = books.find()
+    data = dumps(output)
+    return jsonify({"Status": "OK", "data": data})
+
+@app.route('/books/insert/', methods=['POST'])
+def insert_book():
+    books = mongo.db.books_collection        
+    book = {}
+    result = json.loads(request.get_data(as_text=True))
+    book['title']=result['title']
+    book['author'] = result['author']
+    book['price']=result['price']
+    output = books.insert(book)
+    data = dumps(output)
+    return jsonify({"Status": "OK", "data": data})
 
 @app.route('/books/title/<title>', methods=['GET'])
 def get_book_by_title(title):
@@ -40,6 +45,16 @@ def get_book_by_author(author):
     output = books.find_one({'author': author})
     data = dumps(output)
     return jsonify({"Status": "OK", "data": data})
+
+@app.route('/books/delete/<oid>', methods=['DELETE'])
+def delete_book(oid):
+    books = mongo.db.books_collection
+    try:
+        output = books.delete_one({'_id': ObjectId(oid)})
+    #data = dumps(output,default=json_util.default)
+        return jsonify({"Status": "OK"})
+    except e:
+        return jsonify({"Error":"404"})
 
 if __name__ == '__main__':
     app.run(debug=True)
