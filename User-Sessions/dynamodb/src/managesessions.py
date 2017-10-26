d2567f21f5f1026dc8f3805237d68a5b1b05b209
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-10-26 10:52:58 -0700

    Added new table for user info and insert method.

--- User-Sessions/dynamodb/src/managesessions.py
+++ User-Sessions/dynamodb/src/managesessions.py
@@ -1,21 +1,49 @@
 import boto3
+import uuid
 import os, base64
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
 
-table = dynamodb.Table('Sessions')
+session_table = dynamodb.Table('Sessions')
+user_creds_table = dynamodb.Table('UserCreds')
+
+class User:
+    def __init__(self, first_name, last_name, email, password):
+        self.id = generate_userid()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
 
 def generate_session():
     return base64.b64encode(os.urandom(16))
 
-def create_session_entry():
+def generate_userid():
+    return str(uuid.uuid4())
+
+def add_session(userID):
     sessionvalue = generate_session()
-    table.put_item(
+    session_table.put_item(
        Item={
-            'customerID': '2',
+            'userID': userID,
             'sessionID': sessionvalue
         }
 )
 
+def add_user(user):
+    user_creds_table.put_item(
+        Item={
+            'ID': user.id,
+            'First name': user.first_name,
+            'Last name': user.last_name,
+            'Email': user.email,
+            'Password': user.password
+        }
+    )
 
-create_session_entry()
+
+if __name__ == '__main__':
+    new_user = User("Amita", "Kamat", "abc@gmail.com", "password")
+    add_user(new_user)
+    add_session(new_user.id)
