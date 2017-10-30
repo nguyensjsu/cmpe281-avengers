5f9d5a6704475a5ee0d4829a1bd52fb2bd9e8dac
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-10-30 13:53:36 -0700

    Added methods to fetch user details and session details by User Id.

--- User-Sessions/MongoDB/src/managelogin.py
+++ User-Sessions/MongoDB/src/managelogin.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 import uuid
 import os, base64
 
+from pprint import pprint
+
 client = MongoClient('mongodb://localhost:27017/')
 
 # book-store is the database name
@@ -11,6 +13,7 @@ db = client['book-store']
 user_data = db['Users']
 sessions_data = db['Sessions']
 
+# Class for user details
 class User:
     def __init__(self, first_name, last_name, email, password):
         self.id = generate_userid()
@@ -20,12 +23,17 @@ class User:
         self.password = password
 
 
+# Generates a new session value
 def generate_session():
     return base64.b64encode(os.urandom(16))
 
+
+# Generates a new user ID
 def generate_userid():
     return str(uuid.uuid4())
 
+
+# Adds a session entry to the Sessions table
 def add_session(userID):
     sessionvalue = generate_session()
     result = sessions_data.insert_one(
@@ -36,7 +44,10 @@ def add_session(userID):
     )
     print('session id : ' + str(sessionvalue))
     print('result : ' + str(result.inserted_id))
+    return sessionvalue
 
+
+# Adds user information to the Users table
 def add_user(user):
     result = user_data.insert_one(
             {
@@ -51,7 +62,20 @@ def add_user(user):
     print('result : ' + str(result.inserted_id))
 
 
+# Fetches user details based on user ID
+def get_user(id):
+    return user_data.find_one({'ID': id})
+
+
+# Fetches session details based on user ID
+def get_session(userID):
+    return sessions_data.find_one({'userID': userID})
+
+
+
 if __name__ == '__main__':
     new_user = User("Amita", "Kamat", "abc@gmail.com", "password")
     add_user(new_user)
-    add_session(new_user.id)
+    session_value = add_session(new_user.id)
+    print ( 'User details: ' + str(get_user(new_user.id)))
+    print ( 'Session details: ' + str(get_session(new_user.id)))
