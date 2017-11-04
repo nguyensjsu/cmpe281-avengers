4800fe4fafce13d9675fd9396db9b245709f771b
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-11-04 16:29:48 -0700

    PUT operation to decrement quantity

--- Product Catalog/src/server.py
+++ Product Catalog/src/server.py
@@ -14,14 +14,10 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/books'
 mongo = PyMongo(app)
 
 #  The GET request returns all the books in the database.
-#  The PUT request decrements the quantity of the book in the inventory
 
-@app.route('/v1/books', methods=['PUT','GET'])
+@app.route('/v1/books', methods=['GET'])
 def books():
     books = mongo.db.books_collection
-    #TODO
-    #Decrement Quantity for PUT method
-    if request.method == 'GET':
         try:
             output = books.find()
             data = dumps(output)
@@ -30,15 +26,25 @@ def books():
     return jsonify({"Status": "OK", "data": json.loads(data)})
 
 # The GET request with oid returns a particular document having that Id
+# The PUT request decrements the quantity of the book in the inventory
 
-@app.route('/v1/books/<oid>', methods=['GET'])
+@app.route('/v1/books/<oid>', methods=['GET','PUT'])
 def get_book_by_id(oid):
     books = mongo.db.books_collection
-    try:
-        output = books.find_one({'_id': ObjectId(oid)})
-        data = dumps(output)
-    except Exception as e:
-        return jsonify({"Status":"Error"})
+    if request.method =='PUT':
+        try:
+            output = books.update_one({'_id': ObjectId(oid)},\
+                                      {'$inc':{\
+                                               'Qty': -1\
+                                              }})
+        except Exception as e:
+            return jsonify({"Status": "Error"})
+    elif request.method == 'GET':
+        try:
+            output = books.find_one({'_id': ObjectId(oid)})
+            data = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
     return jsonify({"Status": "OK", "data": json.loads(data)})
 
 if __name__ == '__main__':
