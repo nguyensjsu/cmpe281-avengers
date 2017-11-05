67d44cc997e9eb881f59b80d1c4b25f5a8f88982
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-11-04 21:55:26 -0700

    Modified code to verify user based on email and not id.

--- User-Sessions/MongoDB/src/managelogin.py
+++ User-Sessions/MongoDB/src/managelogin.py
@@ -44,21 +44,22 @@ def generate_userid():
 def add_user(user):
     result = user_data.insert_one(
             {
-                'ID': user.id,
-                'First name': user.first_name,
-                'Last name': user.last_name,
-                'Email': user.email,
-                'Password': user.password
+                'id': user.id,
+                'firstname': user.first_name,
+                'lastname': user.last_name,
+                'email': user.email,
+                'password': user.password
             }
         )
 
 
 # Fetches user details based on user ID
 def get_user(id):
-    return user_data.find_one({'ID': id})
+    return user_data.find_one({'id': id})
 
-def verify_user(id, password):
-    result = user_data.find_one({'ID': id, 'Password' : password})
+# Verify the user credentials
+def verify_user(email, password):
+    result = user_data.find_one({'email': email, 'password' : password})
     if result is None:
         return False
     else:
@@ -66,19 +67,19 @@ def verify_user(id, password):
 
 # Delete user based on user ID
 def delete_user(id):
-    user_data.delete_one({'ID': id})
+    user_data.delete_one({'id': id})
 
 
 # Update user details
 def update_user(user):
     user_data.update_one(
-        {"ID": user.id},
+        {"id": user.id},
             {
             "$set": {
-                'First name': user.first_name,
-                'Last name': user.last_name,
-                'Email': user.email,
-                'Password': user.password
+                'firstname': user.first_name,
+                'lastname': user.last_name,
+                'email': user.email,
+                'password': user.password
             }
         }
     )
@@ -88,44 +89,44 @@ def update_user(user):
 def add_session(session):
     result = sessions_data.insert_one(
         {
-            'userID': session.userID,
-            'sessionID': session.sessionID
+            'userid': session.userID,
+            'sessionid': session.sessionID
         }
     )
 
 
 # Fetches session details based on user ID
 def get_session(userID):
-    return sessions_data.find_one({'userID': userID})
+    return sessions_data.find_one({'userid': userID})
 
 
 # Delete session entry based on User ID
 def delete_session(userID):
-    return sessions_data.delete_one({'userID': userID})
+    return sessions_data.delete_one({'userid': userID})
 
 
 # Update session value for a user
 def update_session(session):
     sessions_data.update_one(
-        {"userID": session.userID},
+        {"userid": session.userID},
             {
                 "$set": {
-                    'sessionID': session.sessionID
+                    'sessionid': session.sessionID
                 }
             }
     )
 
 # Verify if the session for the user is valid
 def verify_session(id, session_value):
-    result = sessions_data.find_one({'userID': id, 'sessionID' : session_value})
+    result = sessions_data.find_one({'userid': id, 'sessionid' : session_value})
     if result is None:
         return False
     else:
         return True
 
 # Verify login and create session
-def verify_login_create_session(id, password):
-    if(verify_user(id, password)):
+def verify_login_create_session(id, email, password):
+    if(verify_user(email, password)):
         # if valid create a session, store it and return session value to the client.
         delete_session(id)
         session = Session(id)
@@ -137,13 +138,15 @@ def verify_login_create_session(id, password):
 
 if __name__ == '__main__':
 
+    ### Tests to test the above methods ###
+
     new_user = User("A", "B", "abc@gmail.com", "password")
     add_user(new_user)
 
     print ( 'User details: ' + str(get_user(new_user.id)))
 
-    print ("Test Invalid login : " + str(verify_login_create_session(new_user.id, 'password1')))
-    print ("Test Valid login : " + str(verify_login_create_session(new_user.id, 'password')))
+    print ("Test Invalid login : " + str(verify_login_create_session(new_user.id, new_user.email, 'password1')))
+    print ("Test Valid login : " + str(verify_login_create_session(new_user.id, new_user.email, 'password')))
 
     new_user.first_name = "A"
     new_user.last_name = "K"
@@ -151,10 +154,11 @@ if __name__ == '__main__':
     print('User details after update: ' + str(get_user(new_user.id)))
 
     delete_session(new_user.id)
-    session_id = verify_login_create_session(new_user.id, 'password')
+    session_id = verify_login_create_session(new_user.id, new_user.email, 'password')
     print ("Test Valid session : " + str(verify_session(new_user.id, session_id)))
     print ("Test Invalid session : " + str(verify_session(new_user.id, 'invalid-sessiond')))
     delete_user(new_user.id)
+    delete_session(new_user.id)
 
     print('User details after delete: ' + str(get_user(new_user.id)))
     print('Session details after delete: ' + str(get_session(new_user.id)))
