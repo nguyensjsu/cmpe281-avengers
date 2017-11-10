925c8e1fbba8e2f7ec7002ca47a0dc3d7ead7d3c
Agarka <31805048+Agarka@users.noreply.github.com>
2017-11-09 21:06:55 -0800

    adding API

--- Financial Data/src/server.py
+++ Financial Data/src/server.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # configure the mondodb database name with 'myorders' and database service uri for the same
 app.config['MONGO_DBNAME'] = 'myorders'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/myorders'
+client = mongo_client()
 
 # Using 'PyMongo' python-mongodb driver 
 mongo = PyMongo(app)
@@ -16,13 +17,23 @@ mongo = PyMongo(app)
 # Using flask_restful Api
 api = Api(app)
 
-class ProductApi(Resource):
-    def get(self):
-        output = mongo.db.myorders_collection.find()
-        data = dumps(output)
-        return jsonify({"Status": "OK", "data": json.loads(data)})
-
-api.add_resource(ProductApi,'/')
+#The GET request returns all the orders in the database.
+   
+  @app.route('/v1/orders', methods=['GET'])
+  def orders():
+ -    if mongo is None:
+ -        return "No server"
+ -    try:
+ -        books = mongo.db.books_collection
+ -        output = books.find()
+ -        data = dumps(output)
+ -    except pymongo.errors.ConnectionFailure as e:
+ -        return jsonify({"Status": "Error","Message":"Connection lost with database server"})
+ -    except pymongo.errors.ServerSelectionTimeoutError as e:
+ -        return jsonify({"Status": "Error","Message":"Could not connect to database server"})
+ -    return jsonify({"Status": "OK", "data": json.loads(data)})
+ +    data = client.get_all()
+ +    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
