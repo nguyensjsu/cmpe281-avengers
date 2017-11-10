a1f6026250044068bbb548547d9ce3d84fe9bc25
Agarka <31805048+Agarka@users.noreply.github.com>
2017-11-09 21:10:22 -0800

    updating the details

--- Financial Data/src/transaction/client.py
+++ Financial Data/src/transaction/client.py
@@ -1,10 +1,45 @@
 from pymongo import MongoClient 
-
 class mongo_client:
-	def __init__(self):
-		self.client = MongoClient()
-		self.db = self.client.myorders
-		self.collection = self.db.myorders_collection
+    def __init__(self):
+        try:
+            self.client = MongoClient()
+            self.db = self.client.books
+            self.collection = self.db.books_collection
+        except pymongo.errors.ConnectionFailure as e:
+            return jsonify({"Status": "Error",\
+                            "Message":"Connection lost with database server"})
+        except pymongo.errors.ServerSelectionTimeoutError as e:
+            return jsonify({"Status": "Error",\
+                            "Message":"Could not connect to database server"})
+
+   
+    def get_all(self):
+        try:
+            output = self.collection.find()
+            data = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
+        return jsonify({"Status": "OK", "data": json.loads(data)})
+
+    def get_one(self,oid):
+        try:
+            output = self.find_one({'_id': ObjectId(oid)})
+            data = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
+        return jsonify({"Status": "OK", "data": json.loads(data)})
+
+    def put_one(self,oid):
+        try:
+            output = books.update_one({'_id': ObjectId(oid)},\
+                                      {'$inc':{\
+                                               'Qty': -1\
+                                              }})
+            data = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
+        return jsonify({"Status":"OK"})
+
 
 	def insert_document(self,id,date,amount):
 		
