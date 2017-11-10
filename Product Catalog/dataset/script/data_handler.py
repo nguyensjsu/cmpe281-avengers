f87906a9c773d35c0b3d85f560ba1bcfef4e56f4
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-11-10 12:16:08 -0800

    Insert dataset from csv into MongoDB

--- Product Catalog/dataset/script/data_handler.py
+++ Product Catalog/dataset/script/data_handler.py
@@ -1,9 +1,22 @@
 import csv
 import json
+import pymongo
+
+connection = pymongo.MongoClient()
+db = connection.test_db
+collection = db.test_collection
+
 
 with open('../new-books.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     rows = list(reader)
+
     with open('mapper.json', 'w') as json_file:
         for row in rows:
             json.dump(row, json_file)
+
+for row in rows:
+    collection.insert(row)
+
+for items in collection.find():
+    print(items) 
