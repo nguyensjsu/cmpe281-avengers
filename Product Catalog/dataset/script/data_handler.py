0e414382786eacc08a85055731585cdc62d25a0d
Pavana Achar <pavana.s.achar1991@gmail.com>
2017-11-10 03:48:20 -0800

    Converting csv to json format

--- Product Catalog/dataset/script/data_handler.py
+++ Product Catalog/dataset/script/data_handler.py
@@ -1,6 +1,9 @@
 import csv
+import json
 
 with open('../new-books.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in reader:
-        print ' '.join(row)       
+    reader = csv.DictReader(csvfile)
+    rows = list(reader)
+    with open('mapper.json', 'w') as json_file:
+        for row in rows:
+            json.dump(row, json_file)
