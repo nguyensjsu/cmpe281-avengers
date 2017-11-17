d6e947c38f7897d7a8dc25d39eb14fdc967c8bbd
Aartee Kasliwal <aarteekasliwal@gmail.com>
2017-11-17 12:06:04 -0800

    Creating client and server for user activity logs

--- User Activity Logs/src/client.py
+++ User Activity Logs/src/client.py
@@ -1,31 +1,43 @@
-from pymongo import MongoClient
-import time
+from pymongo import MongoClient 
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from pymongo import errors
+from flask import jsonify
+import json
+
+'''Mongo Client object to do crud operations on MongoDB'''
 
 class mongo_client:
     def __init__(self):
-        self.client = MongoClient()
-        self.db = self.client.logs
-        self.collection = self.db.logs_collection
-
-    def insert_document(self, log_id, timestamp, message):
-
-        log = {}
-        log["log_id"] = log_id
-        log["timestamp"] = timestamp
-        log["message"] = message
-
-
         try:
-            self.collection.insert_one(log).inserted_id
-            print("Succesfully inserted the document")
+            self.client = MongoClient()
+            self.db = self.client.logs
+            self.collection = self.db.logs_collection
+        except pymongo.errors.ConnectionFailure as e:
+            return jsonify({"Status": "Error",\
+                            "Message":"Connection lost with database server"})
+        except pymongo.errors.ServerSelectionTimeoutError as e:
+            return jsonify({"Status": "Error",\
+                            "Message":"Could not connect to database server"})
 
-        except:
-            print("Document already exists")
+   
+    def get_all(self):
+        try:
+            output = self.collection.find()
+            logs = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
+        return jsonify({"Status": "OK", "logs": json.loads(logs)})
 
-        return
+    def get_one(self,oid):
+        try:
+            output = self.find_one({'_id': ObjectId(oid)})
+            logs = dumps(output)
+        except Exception as e:
+            return jsonify({"Status":"Error"})
+        return jsonify({"Status": "OK", "logs": json.loads(logs)})
 
 
 if __name__ == "__main__":
     client = mongo_client()
-    ts = time.gmtime()
-    client.insert_document(1, time.strftime("%Y-%m-%d %H:%M:%S", ts), "Sample User Activity Log Message")
+    print(client.get_all())
--- User Activity Logs/src/server.py
+++ User Activity Logs/src/server.py
@@ -1,28 +1,32 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+import pymongo
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 import json
+import bson
+from bson.objectid import ObjectId
+from bson import json_util
+from pymongo import errors
+from client import mongo_client
 
 app = Flask(__name__)
-# configure the mondodb database name with 'logs' and database service uri for the same
-app.config['MONGO_DBNAME'] = 'logs'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/logs'
 
-# Using 'PyMongo' python-mongodb driver 
-mongo = PyMongo(app)
+client = mongo_client()
 
-# Using flask_restful Api
-api = Api(app)
+#The GET request returns all the logs in the database.
+ 
+@app.route('/v1/logs', methods=['GET'])
+def logs():
+    logs = client.get_all()
+    return logs
 
-class LogApi(Resource):
-    def get(self):
-        output = mongo.db.logs_collection.find()
-        data = dumps(output)
-        return jsonify({"Status": "OK", "data": json.loads(data)})
-
-api.add_resource(LogApi,'/')
+@app.route('/v1/logs/<oid>', methods=['GET','PUT'])
+def log_by_id(oid):    
+    if request.method =='PUT':
+        log = client.put_one(oid)
+    elif request.method == 'GET':
+        log = client.get_one(oid)
+    return log
 
 if __name__ == '__main__':
     app.run(debug=True)
