dd97ef6623a95b1ba3e801bd326a56a470a2dcba
pavanaachar <pavana.s.achar1991@gmail.com>
2017-12-07 00:05:18 -0800

    Converting Price data to int

--- Integrated-heroku-book-store/Modified-Servers/ProductCatalogServer/dataset/script/data_handler.py
+++ Integrated-heroku-book-store/Modified-Servers/ProductCatalogServer/dataset/script/data_handler.py
@@ -13,8 +13,8 @@ with open('../new-books.csv', 'r') as csvfile:
 
     with open('mapper.json', 'w') as json_file:
         for row in rows:
+            row["Price"] = int(row["Price"])
             json.dump(row, json_file)
-
 for row in rows:
     collection.insert(row)
 
